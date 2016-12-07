9c0e49d1160ff4796fc9d9950cd71b3af082de7a
Vladimir Ivakin <vl.ivakin@yandex.ru>
2016-12-07 14:17:35 +0700

    Update example

--- pokkels_test_example.py
+++ pokkels_test_example.py
@@ -2,13 +2,15 @@ from opticsim import *
 
 
 data_file = open('data.dat', 'w')
-laser_light = light.LinearlyPolarizedHorizontal()
+laser_light = light.LinearlyPolarizedM45()
+
 for i in range(180):
-    pokkels = elements.LinearRetarder(i)
-    for j in range(0, 360, 3):
+    pokkels = elements.LinearRetarder(0, i)
+    for j in range(0, 180):
         linear_polarizer = elements.LinearPolarizer(j)
         s = system.System()
         s.add_elements(pokkels, linear_polarizer)
         changed_light = s.count(laser_light)
-        data_file.write(str(i) + '  ' + str(changed_light.I) + '    ' + str(j) + '\n')
+        data_file.write(str(180 * i + j) + '    ' + str(changed_light.I) + '    ' + str(i / 180) + '\n')
+
 data_file.close()
